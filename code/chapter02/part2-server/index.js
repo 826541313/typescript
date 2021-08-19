d32b3cc69d86348901ebb9b9f07a175880181eee
18733602599 <826541313@qq.com>
2021-08-19 15:58:25 +0800

    修改

--- code/chapter02/part2-server/index.js
+++ code/chapter02/part2-server/index.js
@@ -6,11 +6,11 @@ const fs = require('fs')
 const app = new titbit()
 
 app.get('/bundle.js', async c => {
-    c.res.body = fs.readFileSync('../part2/dist/bundle.js').toString('utf-8')
+    c.res.body = fs.readFileSync('./dist/bundle.js').toString('utf-8')
 })
 
 app.get('/', async c => {
-    c.res.body = fs.readFileSync('../part2/dist/index.html').toString('utf-8')
+    c.res.body = fs.readFileSync('./dist/index.html').toString('utf-8')
 })
 
 
